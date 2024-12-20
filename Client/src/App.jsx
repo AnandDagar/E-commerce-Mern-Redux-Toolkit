82be804e7959f6f5dec56de1bafb921fda2b0535
AnandDagar <ananddagar111@gmail.com>
2024-12-20 12:46:00 +0530

    set design

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/ui/auth/layout";
 
-
 const App = () => {
   return (
-     <div className="flex flex-col overflow-hidden bg-white">
-       <Routes>
-        <Route path="/auth" element={<AuthLayout/>}/>
-       </Routes>
-     </div>
-  )
-}
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
